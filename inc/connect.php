324685b1cc5186854052bcd3c5dafdaa4f43aa54
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-06-18 16:21:00 +0200

    début de création du site
    a ne pas garder = add realisation sera modifié totalement

---
+++ inc/connect.php
@@ -0,0 +1,5 @@
+<?php
+
+$db = new PDO('mysql:host=localhost8888;dbname=noemie;charset=utf8', 'root', '');
+
+?>
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Noemie-escortell</title>
+    <!-- font -->
+    
+	<!-- BOOTSRAP -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <!-- mon css -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav class="blabla navbar navbar-default">
+<div class="container-fluid">
+<!-- Brand and toggle get grouped for better mobile display -->
+<div class="navbar-header">
+  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+    <span class="sr-only">Toggle navigation</span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+  </button>
+  <a class="navbar-brand" href="#">Brand</a>
+</div>  
+  <ul class="nav navbar-nav navbar-right">
+    <li><a href="#">A propos</a></li>
+    <li><a href="#">mes actions</a></li>
+    <li><a href="#">blablabla</a></li>
+    <li><a href="#">tructruc</a></li>
+    <li><a href="#">autre truc</a></li>
+    <li><a href="#">trop bien</a></li>
+    <li><a href="#">contact me</a></li>
+  </ul>
+</div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>
+
+<!-- le logo -->
+<a class="logo" href="#"> </a>
+
+<!-- l'image avec présentation-->
+<a class="navbar-brand" href="#">Brand</a>
+
+<!-- le fil d'actu -->
+<div class="filActua"> 
+<a class="project" href="#"> </a>
+Mon dernier projet 
+</div>
+
+<!-- les rélisations -->
+
+</body>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">	
+</script>
+</html>
