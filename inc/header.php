d4bc64322385328ed37877b7a334f6fab37ed867
Millaa millaa <contact.myriambugnazet@gmail.com>
2016-07-26 14:34:18 +0200

    maj

---
+++ inc/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Noemie-escortell</title>
+    <!-- font -->
+    
+	<!-- BOOTSRAP -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <!-- mon css -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="blabla navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Noemie Escortell</a>
+            </div>  
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">A propos <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Présentation</a></li>
+                        <li><a href="#">Parcours</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>                
+                </li>
+                <li><a href="#">Prestations</a></li>
+                <li><a href="#">Démarches</a></li>
+                <li><a href="#">Réalisations</a></li>
+                <li><a href="#">Paternaires</a></li>
+            </ul>
+        </div><!-- /.container-fluid -->
+    </nav>
+
+   <main>
+
