e1159dc0ef12075c87e5ef6ce943fcb56da09c15
Millaa millaa <contact.myriambugnazet@gmail.com>
2016-07-26 17:23:08 +0200

    maj

--- admin/add_realisation.php
+++ admin/add_realisation.php
@@ -1,44 +1,90 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title></title>
- <!-- meterialize-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+<?php 
 
-</head>
-<body>
+require_once 'inc/connect.php';
+
+$error = []; // array
+$post = []; // array
+$showErrors = false; // Par défaut, on ne veut pas afficher les erreurs
+$success = false; 
+$msgOk = 'Le contact a bien été rajouté';
+$msgError = 'Une erreur malencontreuse s\'est produite';
+
+if(!empty($_POST) && (isset($post))){ //si le formulaire à été soumis
+// On recréer le tableau en le nettoyant des espaces vides et balises HTML
+  foreach($_POST as $key => $value){
+    $post[$key] = trim(strip_tags($value));
+    }
+    if(empty($post['title'])){
+    $error[] = 'il faut donner un titre'; 
+    } 
+    if(empty($post['image'])){
+    $error[] = 'il faut mettre une image';
+    }
+    if(empty($post['content'])){
+    $error[] = 'il faut écrire un texte';
+    }
+
+    if(empty($post['link'])){
+    $error[] = 'il faut mettre un lien';
+    }
+
+    if(count($error) > 0){
+    // Ici il y a des erreurs on les affichera plus tard
+    $showErrors = true;
+    }
+    else {
+    $res = $db->prepare('INSERT INTO achievements (title, image, content, link) VALUES(:title, :image, :content, :link');
+
+    $res->bindValue(':title', $post['title']);
+    $res->bindValue(':image', $post['image']);
+    $res->bindValue(':content', $post['content']);
+    $res->bindValue(':link', $post['link']);
+  
+    if($res->execute()){ // Si la requete s'execute correctement
+        $success = true;
+    }
+  }
+}
+?>
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ajouter des réalisations</title>
+  </head>
+  <body>
+    <?php 
+        if($showErrors){
+            echo $msgError .' <br />';
+            echo implode(' <br />', $error);
+
+        }
+        if($success){
+            echo $msgOk;
+        }
+    ?>  
+    <form method="POST">
+        <label for="title">Titre</label>
+        <input type="text" name="title" id="title" placeholder="Votre titre...">
+
+        <br>
+        <label for="image">Image</label>
+        <input type="text" name="image" id="image" placeholder="Votre image...">
+
+        <br>
+        <label for="link">Lien</label>
+        <input type="text" name="link" id="link" placeholder="Votre lien...">
+
+        <br>
+        <label for="content">Texte</label>
+        <input type="text" name="content" id="content" placeholder="texte">
+
+        <br>
+        <input type="submit" value="Envoyer">
+
+      </form>
+    </body>
 
 </body>
-  <div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-          <label for="first_name">First Name</label>
-        </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate">
-          <label for="last_name">Last Name</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-          <label for="disabled">Disabled</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
-          <label for="password">Password</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
-          <label for="email">Email</label>
-        </div>
-      </div>
-    </form>
-  </div>
 </html>
---
+++ admin/realisations.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Les réalisations</title>
+  </head>
+  <body>
+
+  <h1><a href="add_realisation.php">Ajouter des réalisations</a></h1>
+  <h1> Modifier des réalisations </h1>
+  <h1> Supprimer des réalisations </h1>
+
+
+  </body>
+  </html>
