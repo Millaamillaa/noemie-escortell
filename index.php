8b0f893ac6740af66435c16543c1d8d1b4ccfbf7
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-09-07 02:13:43 +0200

    maj

--- index.php
+++ index.php
@@ -13,77 +13,100 @@
                 <div class="noemie col-xs-6 col-md-5">
                     <img src="img/p1-logo.png" alt="logo-intro">
                 </div>
-            </div>
+            </div> 
         </div>
     </section>
     
 
     <!-- bloc 2-->
     <section id="bloc2">
-        <div class="nomargin container">
+        <div class="container">
             <div class="row">
-                <div class="textbloc2 col-xs-6 col-md-4">
+                <div class="textbloc2 col-xs-6 col-md-6">
                     <br>
                     <p> &nbsp; ACCOMPAGNEMENT AU MONTAGE 
                     <br> DE PROJETS EUROPÉENS ET DE COOPÉRATION </p>
                 </div>
-                <div class="textbloc2 col-xs-6 col-md-3">
+                <br>
+                <div class="textbloc2 col-xs-6 col-md-6">
                     <img src="img/p1-barre-separation.png" alt="separation">
                 </div>
-                <div class="textsuite col-xs-6 col-md-3">     
+                <br>
+                <div class="textsuite col-xs-6 col-md-6">     
                     <p> 
                         <img src="img/triangle-g.png" alt="triangle"> 
-                        &nbsp; Coopérer/Innover/Se Développer &nbsp;
+                        &nbsp; Coopérer / Innover / Se Développer &nbsp;
                         <img src="img/triangle-d.png" alt="triangle">
                     </p>
                 </div>  
-                <div class="nomarginright paillettesdroite col-xs-6 col-sm-3">
-                    <img src="img/p1-paillette-droite.png" alt="pailletes">
+            </div>
+        </div>
+    </section>
+
+    <!-- bloc 3-->
+    <section id="bloc3">
+        <div class="container">
+            <div class="row imgbloc3">
+                <div class="col-xs-6 col-md-8">
+                    <img src="img/p2-barreseparationbold.png" alt="separation">
+                </div>
+                <div class="textbloc3 col-xs-6 col-md-8">
+                    <br>
+                    <p> POUR S'OUVRIR SUR LE MONDE
+                    <br> POUR ALLER PLUS LOIN, ENSEMBLE </p>
+                    <br>
+                </div>
+                <div class="col-xs-6 col-md-8">
+                     <img src="img/p2-barreseparationregular.png" alt="separation">
+                </div> 
+            </div>
+        </div>
+    </section>
+    <div id="separateurorange"></div>
+    
+     <!-- bloc 4-->
+    <section id="bloc4">
+        <div class="container">
+            <div class="row">
+             <h1 class="titlebloc4">DERNIÈRES RÉALISATIONS </h1>
+
+                <!-- image 1 -->
+                <div class="col">
+                    <div class="textbloc4">
+                        <p> ELABORATION DE STRATÉGIE 
+                            <br> EUROPÉENNE APPUI TECHNIQUE À LA 
+                            <br> RÉDACTION ET AU MONTAGE 
+                        </p>
+                     </div>
+                    <img src="img/p3-barre-orange.png" alt="separation">
+                    <img src="img/p3-vignette-1.jpg" alt="vignette">
+                </div>
+
+            <!-- image 2 -->
+                <div class="col">
+                    <img src="img/p3-vignette-2.jpg" alt="vignette">
+                    <img src="img/p3-barre-orange.png" alt="separation">
+                    <div class="textbloc4">
+                        <p> APPUI TECHNIQUE SUR LA 
+                            <br> COMMUNICATION ET LA GESTION 
+                            <br> ADMINISTRATIVE
+                        </p>
+                     </div>
+                </div>
+
+            <!-- image 3 -->
+                <div class="col">
+                    <div class="textbloc4">
+                        <p> APPUI TECHNIQUE À L'ÉCRITURE, AU
+                            <br> DÉPÔT ET SUIVI ADMINISTRATIF
+                        </p>
+                    </div>
+                <img src="img/p3-barre-orange.png" alt="separation">
+                <img src="img/p3-vignette-3.jpg" alt="vignette">
                 </div>
             </div>
         </div>
     </section>
-<br><br><br><br><br><br><br>
-  
-
-<br><hr><hr><br><br><br><br><br><br><br><br><br>
-    
-    
-        
-        <img src="img/p2-background.jpg" alt="fond">
-    </section>
-    <br>
-    <hr>
-
-    <img src="img/p2-barreseparationbold.png" alt="separation">
-
-    <br>
-    <hr>
-    <img src="img/p2-barreseparationregular.png" alt="separation">
-
-    <hr>
-
-    <img src="img/p3-barre-orange.png" alt="orange">
-
-    <br><hr>
-
-    <!-- le fil d'actu -->
-    <div class="filActua"> 
-    <a class="project" href="#"> </a>
-    Mon dernier projet 
-    </div>
-
-    <!-- les rélisations -->
-    <div class="lastprojetcs">
-        <div class="one" style="background-image: url(http://www.enceinte.com/blog/wp-content/uploads/2011/04/pub-evian.png); height: 300px; width: 600px; border: 1px solid black;"> 
-        <p>Elaboration de strategie europeene blabla</p>
-        </div>
-        <br>
-        <div class="one" style="background-image: url(http://www.monbebe.com/video/vignette/932.jpeg); height: 400px; width: 600px; border: 1px solid black;"> Appuye technique sur la communication et la gestion blabla
-        </div>
-        <br>
-        <div class="one" style="background-image: url(http://lareclame.fr/wp-content/uploads/2016/04/6.-T-shirt-03.jpg); height: 400px; width: 600px; border: 1px solid black;"> Appui technique au dépôt blabla
-        </div>
 
 
 
