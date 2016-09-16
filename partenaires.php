956f729c12a3b75cda7a42211fa14c4ad138ae6e
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-09-16 14:45:12 +0200

    stucture partenaires

--- partenaires.php
+++ partenaires.php
@@ -1,6 +1,198 @@
 <?php 
 include_once 'inc/header.php';
 ?>
+<!-- bloc 1-->
+    <section id="bloc1">
+        <p class="titlebloc"> PARTENAIRES </p>
+        <img src="img/p3-vignette-2.jpg" alt="vignette"> <!-- image banderole -->
+        
+        <div class="container">
+            <div class="row">
+                <!-- presta 1 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 2 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 3 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <!-- presta 4 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 5 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 6 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <!-- presta 7 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 8 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 9 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <!-- presta 10 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 11 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 12 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <!-- presta 13 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 14 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 15 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <!-- presta 16 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 17 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+                <!-- presta 18 --> 
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="mespartenaires">
+                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- partenaire 1 -->
+                        <p class="titlepartenaire"> BLABLABLA </p>
+                        <p class="textpartenaire"> BLABLABLA </p>
+                        <a href="http://www.w3schools.com">entrer le nom du site www.blabla.com</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php  
 include_once 'inc/footer.php';
 ?>
