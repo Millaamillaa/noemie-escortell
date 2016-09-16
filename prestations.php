82aa91b5fc6d3567bd11c1114b6943153f1fa848
lilicracra <contact.lisaratto@gmail.com>
2016-09-16 14:46:41 +0200

    contenu presta

--- prestations.php
+++ prestations.php
@@ -3,37 +3,37 @@ include_once 'inc/header.php';
 ?>
 
 <!-- bloc 1-->
-    <section id="bloc1">
+    <section id="">
         <p class="titlebloc"> PRESTATIONS </p>
-        <img src="img/p3-vignette-2.jpg" alt="vignette"> <!-- image banderole -->
+        <img src="img/prestation-soulignage-titre.png" alt="vignette"> <!-- image banderole -->
         
         <div class="container">
             <div class="row">
                 <!-- picto 1 --> 
                 <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
                     <div class="mespicto">
-                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- picto 1 -->
-                        <p class="titlepresta"> BLABLABLA </p>
-                        <p class="textpresta"> BLABLABLA </p>
-                        <a href="lienverslalecture du texte"><img src="rainbow.gif"></a> <!-- en savoir plus image -->
+                    	<img class="lineimage" src="img/prestation-picto-1.png" alt="vignette"> <!-- picto 1 -->
+                        <p class="titlepresta"> DOMAINES DE COMPÉTENCE </p>
+                        <p class="textpresta"> Gestion et coordination de projet ... </p>
+                        <a href="lienverslalecture du texte"><img src="img/btn-ensavoirplus.png"></a> <!-- en savoir plus image -->
                     </div>
                 </div>
                 <!-- picto 2 --> 
                 <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
                     <div class="mespicto">
-                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- picto 2 -->
-                        <p class="titlepresta"> BLABLABLA </p>
-                        <p class="textpresta"> BLABLABLA </p>
-                        <a href="lienverslalecture du texte"><img src="rainbow.gif"></a> <!-- en savoir plus image -->
+                    	<img class="lineimage" src="img/prestation-picto-2.png" alt="vignette"> <!-- picto 2 -->
+                        <p class="titlepresta"> MÉTHODOLOGIE </p>
+                        <p class="textpresta"> Par le biais de la formation et de ... </p>
+                        <a href="lienverslalecture du texte"><img src="img/btn-ensavoirplus.png"></a> <!-- en savoir plus image -->
                     </div>
                 </div>
                 <!-- picto 3--> 
                 <div class="col-xs-12 col-sm-9 col-md-6 col-lg-4">
                     <div class="mes picto">
-                    	<img class="lineimage" src="img/p3-vignette-1.jpg" alt="vignette"> <!-- picto  3-->
-                        <p class="titlepresta"> BLABLABLA </p>
-                        <p class="textpresta"> BLABLABLA </p>
-                        <a href="lienverslalecture du texte"><img src="rainbow.gif"></a> <!-- en savoir plus image -->
+                    	<img class="lineimage" src="img/prestation-picto-3.png" alt="vignette"> <!-- picto  3-->
+                        <p class="titlepresta"> DÉMARCHE </p>
+                        <p class="textpresta"> Parce qu'un projet réussi est ... </p>
+                        <a href="lienverslalecture du texte"><img src="img/btn-ensavoirplus.png"></a> <!-- en savoir plus image -->
                     </div>
                 </div>
             </div>
