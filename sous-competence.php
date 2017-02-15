b1ae5805e192332e7e30a2fe63ffabd9e9bc3758
Millaa Millaa <contact.myriambugnazet@gmail.com>
2017-02-15 22:26:12 +0100

    add sous competence

---
+++ sous-competence.php
@@ -0,0 +1,28 @@
+<?php 
+require_once 'inc/header.php';
+?>
+ 
+<!-- bloc 1-->
+    <section id="prestation">
+        <p class="titleunderview"> DOMAINES DE COMPÉTENCE </p>
+        <img src="img/prestation-soulignage-titre.png" alt="vignette"> <!-- image banderole -->
+        <div class="insideunderview">
+            <div class="container">
+                <div class="row">
+                    <!-- picto 1 --> 
+                    <div class="centerunderview">
+                    	<img class="prestapicto" src="img/prestation-picto-1.png" alt="vignette"> <!-- picto 1 -->
+                        <p class="titlepresta"> COOPÉRATION INTERRÉGIONALE ET EUROPEENNE </p>
+                        <p class="textunder"> ► Gestion et coordination de projet <br>
+                        ► Formation des acteurs de l’Economie Sociales et Solidaire   <br>
+                        ► Etudes et assistance à maîtrise d’ouvrage <p>
+                        <br>
+                        <a href="demarches.php" class="btn btn-warning btn-lg active" role="button" aria-pressed="true">Retour aux démarches</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<?php  
+require_once 'inc/footer.php';
+?>
